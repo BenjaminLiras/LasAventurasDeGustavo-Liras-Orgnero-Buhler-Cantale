8d40fe38b47fc5d314c00b83e8a3c9a435c1d0ec
Cantale08 <santino.cantale@gmail.com>
2026-09-07 16:51:25 -0300

    Segundo commit

--- las-aventuras-de-gustavo/gustavo.gd
+++ las-aventuras-de-gustavo/gustavo.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var doubleSalto = true
 
 
 func _physics_process(delta: float) -> void:
@@ -13,6 +14,11 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("ui_accept") and !is_on_floor() and doubleSalto:
+		velocity.y = JUMP_VELOCITY 
+		doubleSalto = false
+	if is_on_floor() and !Input.is_action_just_pressed("ui_accept"):
+		doubleSalto = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
