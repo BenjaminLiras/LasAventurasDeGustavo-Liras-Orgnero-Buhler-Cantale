15a449b2bb80a91c294f172bf68737a8f3fc775e
bula393 <alumno26.buhler.santiago@ipm.edu.ar>
2026-09-13 02:11:21 -0300

    empezada implementacion personaje principal

--- las-aventuras-de-gustavo/gustavo.gd
+++ las-aventuras-de-gustavo/gustavo.gd
@@ -4,13 +4,14 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var doubleSalto = true
+var direction
+var dashActivo = false
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -19,13 +20,27 @@ func _physics_process(delta: float) -> void:
 		doubleSalto = false
 	if is_on_floor() and !Input.is_action_just_pressed("ui_accept"):
 		doubleSalto = true
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	direction = Input.get_axis("ui_left", "ui_right")
+	if !dashActivo:
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	if Input.is_action_just_pressed("dash"):
+		dash()
 	move_and_slide()
+	Global.setVelocidad(velocity)
+	Global.setDireccion(direction)
+	Global.setDash(dashActivo)
+
+
+
+
+
+func dash() -> void:
+	if !dashActivo:
+		velocity.x = velocity.x + 150
+		dashActivo = true	
+	await get_tree().create_timer(3)
+	dashActivo = false
+	velocity = Vector2.ZERO	
